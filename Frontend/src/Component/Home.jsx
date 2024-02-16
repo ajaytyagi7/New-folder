c2cdc765f30f0c32c7865b7698c3e05b5b752f95
Ajay Kumar Tyagi <151626208+ajaytyagi7@users.noreply.github.com>
2024-02-16 22:28:04 +0530

    update

--- Frontend/src/Component/Home.jsx
+++ Frontend/src/Component/Home.jsx
@@ -55,19 +55,19 @@ const Home = () => {
 
           <div className=' container text-center mt-4 overflow-hidden '>
             <div className='row gx-1 ' style={{gap:20}}>
-              <div className='card col'>
+              <div className='card col home-card'>
                 <img className='img-fluid' style={{height:200}}  src="https://img.freepik.com/premium-photo/hand-holding-silver-pen-against-grey-background-with-vignette_1134-82543.jpg?size=626&ext=jpg&ga=GA1.1.913293683.1703873181&semt=ais" alt="" />
                 <h5 className='mt-2'>Design & Creative</h5>
               </div>
-              <div className='card col'>
+              <div className='card col home-card'>
                 <img className='img-fluid' style={{height:200}} src="https://img.freepik.com/free-photo/multiethnic-team-sharing-ideas-about-industrial-project-using-two-monitors-with-d-gears-metalic_482257-2641.jpg?size=626&ext=jpg&ga=GA1.1.913293683.1703873181&semt=ais" alt="" />
                 <h5 className='mt-2'>Design & Development</h5>
               </div>
-              <div className='card col '>
+              <div className='card col home-card'>
                 <img className='img-fluid' style={{height:200}} src="https://img.freepik.com/free-photo/medium-shot-woman-working-laptop_23-2150323516.jpg?size=626&ext=jpg&ga=GA1.1.913293683.1703873181&semt=ais" alt="" />
                 <h5 className='mt-2'>Web Development</h5>
               </div>
-              <div className='card col '>
+              <div className='card col home-card'>
                 <img className='img-fluid' style={{height:200}} src="https://img.freepik.com/free-photo/front-view-pretty-woman-showing-diagrams-working-office_140725-109240.jpg?size=626&ext=jpg&ga=GA1.1.913293683.1703873181&semt=ais" alt="" />
                 <h5 className='mt-2'>Sales And Marketing</h5>
               </div>
